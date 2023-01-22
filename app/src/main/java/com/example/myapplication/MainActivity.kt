ed7f705c81c8941d5a5e736c051a3a1f012bf344
Priyanshu Chakarvarti <priyanshu7222@gmail.com>
2023-01-22 14:18:09 +0530

    java compiler

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sun.tools.javac.file.JavacFileManager
 import com.sun.tools.javac.main.JavaCompiler
+import com.sun.tools.javac.tree.JCTree
+import com.sun.tools.javac.util.Context
 import com.sun.tools.javac.util.List
+import java.io.File
 import javax.tools.JavaFileObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var compiler: JavaCompiler
-        var obj:JavaFileObject = JavaFileObject()
-        compiler.compile(listOf(obj) as List<JavaFileObject>?)
+
+        val compiler = JavaCompiler.instance(Context())
+        compiler.keepComments = true
+
+        val testSrc = System.getProperty("test.src")
+        val fm = JavacFileManager(Context(), false, null)
+        val f: JavaFileObject = fm.getJavaFileObject(testSrc + File.separatorChar + "T4910483.java")
+
+        val cu: JCTree.JCCompilationUnit = compiler.parse(f)
+        val classDef: JCTree = cu.getTypeDecls().head
+        val commentText: String = cu.docComments.getCommentText(classDef)
+
+        val expected = "Test comment abc*\\\\def" // 4 '\' escapes to 2 in a string literal
+
+        if (expected != commentText) {
+            throw AssertionError("Incorrect comment text: [$commentText], expected [$expected]")
+        }
     }
 }
