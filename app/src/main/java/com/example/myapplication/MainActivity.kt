9f99a3809cedf8144669c60f3634d786717d0d1b
Priyanshu Chakarvarti <priyanshu7222@gmail.com>
2023-01-22 14:08:19 +0530

    java compiler

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sun.tools.javac.main.JavaCompiler
+import com.sun.tools.javac.util.List
+import javax.tools.JavaFileObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var compiler: JavaCompiler
+        var obj:JavaFileObject = JavaFileObject()
+        compiler.compile(listOf(obj) as List<JavaFileObject>?)
     }
 }
